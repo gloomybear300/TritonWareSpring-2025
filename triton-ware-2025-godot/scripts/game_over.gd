972b57317e49e7e9f8aaaf9fdb1be2ec1a5fd10a
gloomybear300 <s2hill@ucsd.edu>
2025-04-10 16:01:11 -0700

    created game over

--- triton-ware-2025-godot/scripts/game_over.gd
+++ triton-ware-2025-godot/scripts/game_over.gd
@@ -3,12 +3,19 @@ extends Control
 @export var main_menu_scene : PackedScene
 @export var game_scene : PackedScene
 
-func show_game_over(score: int):
-	$CanvasLayer/ScoreLabel.text = "Score: %d" % score
-	self.visible = true
-	
-func _on_retry_button_pressed():
-	get_tree().change_scene_to_packed(game_scene)
+func _ready():
+	show_game_over(GameState.final_time)
 
-func _on_main_menu_button_pressed():
-	get_tree().change_scene_to_packed(main_menu_scene)
+func show_game_over(score: float):  # Changed from int to float
+	$CanvasLayer/ScoreLabel.text = "Time: %.2f seconds" % score
+	self.visible = true
+
+func _on_retry_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+	
+
+
+func _on_main_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	pass # Replace with function body.
